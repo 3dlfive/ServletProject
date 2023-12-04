0e66755af2d3571335231cdd7f8fb57fed225948
3lfive <denystry@gmail.com>
2023-12-04 15:24:23 -0500

    feat:feat add template, static content for Users page

---
+++ src/main/java/Servlets/StylesServlet.java
@@ -0,0 +1,41 @@
+package Servlets;
+
+import Helpers.ResourcesOps;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+public class StylesServlet extends HttpServlet {
+    private final String root;
+
+    public StylesServlet(String root) {
+        this.root = root;
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String prefix = ResourcesOps.dirUnsafe(root);
+        String fileName = req.getPathInfo();
+        String fullName = prefix + fileName;
+        Path path = Paths.get(fullName.substring(1));
+        Stream<String> lines = Files.lines(path);
+
+        try (PrintWriter w = resp.getWriter()) {
+            lines.forEach(w::println);
+        }
+
+
+    }
+
+}
--- src/main/java/Servlets/Users/UsersServlet.java
+++ src/main/java/Servlets/Users/UsersServlet.java
@@ -1,14 +1,20 @@
 package Servlets.Users;
 
+import Helpers.ResourcesOps;
 import Servlets.Likes.LikedDB;
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.SQLException;
+import java.util.HashMap;
 
 public class UsersServlet extends HttpServlet {
     private final DaoUsersList users;
@@ -21,21 +27,37 @@ public class UsersServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        try (PrintWriter w = resp.getWriter()) {
-            w.println("<html><body><table>");
-//            users.get().forEach(x -> w.println(x.toHtml()));
-            w.println(users.find(counter).get().toHtml());
-            w.println("</table></body></html>");
+//        try (PrintWriter w = resp.getWriter()) {
+//            w.println("<html><body><table>");
+////            users.get().forEach(x -> w.println(x.toHtml()));
+//            w.println(users.find(counter).get().toHtml());
+//            w.println("</table></body></html>");
+//        } catch (SQLException e) {
+//            throw new RuntimeException(e);
+//        }
+
+            Configuration cfg = new Configuration(Configuration.VERSION_2_3_32);
+            cfg.setDirectoryForTemplateLoading(new File(ResourcesOps.dirUnsafe("static/templates")));
+            HashMap<String, Object> data = new HashMap<>();
+        try {
+            data.put("user",users.find(counter).get());
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-
+        try (PrintWriter w = resp.getWriter()) {
+            Template template = cfg.getTemplate("profile.ftl");
+            template.process(data, w);
+        } catch (TemplateException e) {
+            throw new RuntimeException(e);
+        }
     }
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        boolean decision = Boolean.parseBoolean(req.getParameter("des_button"));
-        String name = req.getParameter("name");
+        boolean decision = Boolean.parseBoolean(req.getParameter("des_button").substring(0,req.getParameter("des_button").indexOf(",")));
+        System.out.println(decision);
+        String name = req.getParameter("des_button").substring(req.getParameter("des_button").indexOf(",")+1);
+        System.out.println(name);
         if (decision) likes.put(users.findFirst(name).get());
 
         if(counter == users.size()-1){counter=0;
